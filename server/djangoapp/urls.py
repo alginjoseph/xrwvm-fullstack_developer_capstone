1c3c170f6f2db4e21e6fa57c3216bd681aa841e4
alginjoseph <algin555@gmail.com>
2026-02-24 16:06:11 -0500

    logout implemented register implemented

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,6 +7,8 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
+    path(route='logout', view=views.logout_request, name='logout'),
+    path(route='register', view=views.registration, name='registration'),
     # # path for registration
 
     # path for login
